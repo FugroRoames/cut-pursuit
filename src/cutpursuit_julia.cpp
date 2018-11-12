f621b1a6850a195e145de2806e2c7fda09815641
Josh Christie <joshua.christie@roames.com.au>
2018-11-12 10:30:22 +1000

    Tidy up unused functions

--- src/cutpursuit_julia.cpp
+++ src/cutpursuit_julia.cpp
@@ -1,35 +1,8 @@
 #include <string>
 #include <../include/API.h>
 
-#include <boost/type_index.hpp>
-#include <iostream>
-
 #include "jlcxx/jlcxx.hpp"
-#include "jlcxx/functions.hpp"
 #include "jlcxx/array.hpp"
-#include "jlcxx/tuple.hpp"
-
-// #include "boost/tuple/tuple.hpp"
-// typedef boost::tuple< std::vector< std::vector<uint32_t> >, std::vector<uint32_t> > Custom_tuple;
-
-// Testing commands:
-// open julia
-// > using CxxWrap; @wrapmodule(joinpath("/home/josh/dev/superpoint_graph/partition/cut-pursuit/src/", "libcpjl"))
-// > cut_pursuit(rand(3,10), rand(10,2), Int32.(rand(1:10, 10)), Int32.(rand(1:10,10)), ones(Float64,10))
-
-// template <typename T>
-// jlcxx::Array<float> transform_julia(std::vector<T> in)
-// {
-//     jlcxx::Array<float> out[in.size()];
-//     std::transform(in.begin(), in.end(), out, [](const double d) { return 0.5*d; });
-//     return out;
-// }
-
-struct Component
-{
-    Component(std::vector<uint32_t> comp) : component(comp.begin(), comp.end()){}
-    std::vector<uint32_t> component;
-};
 
 struct Result
 {
@@ -41,13 +14,9 @@ struct Result
     std::vector<uint32_t> in_components;
     std::vector<std::vector<uint32_t>> components;
 
-    ~Result() { std::cout << "Destroying Result with message " << std::endl; }
+    ~Result() { std::cout << "Destroying CutPursuit Result" << std::endl; }
 };
 
-
-
-// jlcxx::ArrayRef<std::vector<unsigned int>, 2>
-// std::tuple<jlcxx::ArrayRef<float, 1>, jlcxx::ArrayRef<uint32_t, 1>, jlcxx::ArrayRef<size()uint32_t, 2>>
 Result cutpursuit_seg(const uint32_t n_nodes,
                       const uint32_t n_edge,
                       const uint32_t n_obs,
@@ -61,6 +30,7 @@ Result cutpursuit_seg(const uint32_t n_nodes,
                       float speed,
                       float verbose)
 {
+    //read data and run the L0-cut pursuit partition algorithm
     float obs_data[obs.size()];
     std::transform(obs.begin(), obs.end(), obs_data, [](double d) { return static_cast<float>(d);});
 
@@ -76,43 +46,22 @@ Result cutpursuit_seg(const uint32_t n_nodes,
     float node_weight_data[source.size()];
     std::transform(node_weight.begin(), node_weight.end(), node_weight_data, [](double d) { return static_cast<float>(d);});
 
-    // float solution [n_ver * n_obs];
     std::vector<float> solution(n_nodes * n_obs, 0.0f);
     std::vector<uint32_t> in_component(n_nodes, 0);
     std::vector< std::vector<uint32_t> > components(1, std::vector<uint32_t>(1, 0.f));
 
     CP::cut_pursuit<float>(n_nodes, n_edge, n_obs, obs_data, source_data, target_data, edge_weight_data, node_weight_data, solution.data(), in_component, components, lambda, mode, speed, verbose);
 
-    // auto solution_out = jlcxx::ArrayRef<float, 1>(solution.data(), solution.size());
-    // auto in_component_out = jlcxx::ArrayRef<uint32_t, 1>(in_component.data(), in_component.size());
-    // auto components_out = jlcxx::ArrayRef<uint32_t, 2>(components.data(), components.size());
-    // auto out = jlcxx::make_julia_array(components.data(), components.size(), components[0].size());
-
     return Result(solution, in_component, components);
 }
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
 {
-    // jlcxx::static_type_mapping<StdVector>::set_julia_type((jl_datatype_t*)jlcxx::julia_type("StdVector"));
-
     mod.method("cut_pursuit", &cutpursuit_seg);
 
+    // TODO Work out how to return std::vector<std::vector<>>
     mod.add_type<Result>("Result")
         .method("in_components", [](Result& f) { return std::make_tuple(&(f.in_components[0]),
                                                                              &(f.in_components[0])+ f.in_components.size()/sizeof(uint32_t),
                                                                              f.in_components.capacity()); });
-        // .method("components_test", [](Result& f) { return std::make_tuple(&(f.components[0]),
-        //                                                                   &(f.components[0])+ f.components.size()),
-        //                                                                   f.components.capacity()); })
-        // .method("components", [](Result& f, int i) { return f.components.size()); })
-        // .method("num_components", [](Result& f) { return f.components.size(); })
-        // .method("in_components_disp", [](Result& f) {
-        //     for (int i = 0; i < f.in_components.size(); i++)
-        //     {
-        //         std::cout << i << ": " << f.in_components[i] << std::endl;
-        //     }
-        //     return "hi";
-        //     });
-    //     .method("in_components", [](Result& f) { return jlcxx::ArrayRef<uint32_t>(&(f.in_components[0]), f.in_components.size()); })
-
 }
